e494d58d786496fd57c6883fe4f0347c91a9f813
Dalton Carter <Dj_Carter_Gottenbos@yahoo.com>
2023-04-07 13:07:23 -0700

    Starting first section.

--- Set Up/Hello Test/Main.cpp
+++ Set Up/Hello Test/Main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main() {
+    int num;
     cout << "Hello Testing" << endl;
+    cout << "Enter your favorite number.";
+    cin >> num;
+    cout << "Your favorite number is, " << num << endl;
     return 0;
 }
